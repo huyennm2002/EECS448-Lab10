6ba7de93dac532a55ab2a1c32621b281032db9b2
huyennm2002 <52208231+huyennm2002@users.noreply.github.com>
2022-05-05 17:03:32 -0500

    Add files via upload

--- CreatePosts.php
+++ CreatePosts.php
@@ -1,14 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Posts</title>
+    <title>Create New Post</title>
     <style>
+        body {
+            font-family: Verdana, sans-serif;
+            font-size: 14px;
+            align-items: center;
+        }
+
+        form {
+            align-items: center;
+            background-color: #ffffff;
+            color: black;
+            line-height: 24px;
+            border-radius: 10px;
+            margin: 0px auto;
+            width: 75%;
+            text-align: center;
+        } 
+        .userInfo {
+            background-color: #ffffff;
+            border-color: #ced4da;
+            border-radius: 4px 4px 0px 0px;
+            border-style: solid;
+            border-width: 1px;
+            color: #495057;
+            font-family: -apple-system;
+            line-height: 24px;
+            margin: 0px 0px -1px; 
+            padding: 10px;
+            font-size: 20px;
+        }
+
         button {
             align-items: flex-start;
             background-color: #fa8816;
-            border-color: #fa8816;
             border-radius: 20px;
             border-style: solid;
             border-width: 1px;
@@ -19,27 +49,43 @@
             padding: 8px 16px;
             text-align: right;
         }
+
         .noti {
-            margin: 20px 60px;
-            font-size: 30px;
-        }
-        a {
-            text-decoration: none;
+            text-align: center;
+            color: red;
         }
     </style>
 </head>
 <body>
     <button style="border-radius: 0px; background-color:grey;"><a style="color: white;" href="index.html">Home Page</a></button>
+    <form class="form" action="CreatePosts.php" method="POST">
+        <div>
+            <h2> Create New Post </h2>
+            <p></p>
+            <div class="userInfo">
+                <span></span>
+                <label for="username">Username</label>
+                <input type="text" name="username" value="" placeholder="Enter Username">
+            </div>
+            <div class="userInfo">
+                <label for="post">Your post:</label>
+                <br>
+                <textarea style="width:75%; height: 400px;" name="post" value = "" placeholder="Write your post here"></textarea>
+            </div>
+            <br>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
     <br>
     <?php
         $user_id = $_POST['username'];
         $content = $_POST['post'];
         $mysqli = new mysqli("mysql.eecs.ku.edu", "m449n496", "Ait3vahs", "m449n496");
         // //if username is empty
-        if($user_id == '') echo "Please submit your username.";
+        if($user_id == '') echo "<div class='noti'>Please submit your username.</div>";
         else{
             if ($content == '') {
-                printf("There are no posts to save. Please write your post.");
+                echo "<div class='noti'>There are no posts to save. Please write your post.</div>";
                 exit();
             }
             //check connection
@@ -53,7 +99,7 @@
                 $mysqli->query($query);
                 $result = $mysqli->query($query);              
                 //Username is taken
-                if ($result->num_rows == 0) echo "User does not exist.";
+                if ($result->num_rows == 0) echo "<div class='noti'>User does not exist.</div>";
                 // Username is not taken
                 else{
                     // Insert username into Users table
@@ -68,6 +114,5 @@
             }
         }
     ?> 
-    <button><a href='CreatePosts.html'>Create new Post</a></button>   
 </body>
 </html>
--- CreateUser.php
+++ CreateUser.php
@@ -1,16 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
+    <style>
+        body {
+            font-family: Verdana, sans-serif;
+            font-size: 14px;
+            align-items: center;
+        }
+
+        form {
+            align-items: center;
+            background-color: #ffffff;
+            color: black;
+            line-height: 24px;
+            width: 50%;
+            margin: 90px auto;
+            border-radius: 10px;
+            padding: 15px;
+            text-align: center;
+        }
+        .userInfo {
+            background-color: #ffffff;
+            border-color: #ced4da;
+            border-radius: 4px 4px 0px 0px;
+            border-style: solid;
+            border-width: 1px;
+            color: #495057;
+            font-family: -apple-system;
+            line-height: 24px;
+            margin: 0px 0px -1px;
+            padding: 10px;
+        }
+
+        button {
+            align-items: flex-start;
+            background-color: #fa8816;
+            border-color: #fa8816;
+            border-radius: 20px;
+            border-style: solid;
+            border-width: 1px;
+            color: #ffffff;
+            font-family: -apple-system;
+            font-size: 20px;
+            line-height: 30px;
+            padding: 8px 16px;
+            text-align: right;
+        }
+
+        .noti {
+            text-align: center;
+            color: red;
+        }
+    </style>
 </head>
 <body>
+    <button style="border-radius: 0px; background-color:grey;"><a style="color: white;" href="index.html">Home Page</a></button>
+    <form class="form" action="CreateUser.php" method="POST">
+        <div>
+            <h2> Create New User </h2>
+            <p></p>
+            <div class="userInfo">
+                <span></span>
+                <label for="username">Username</label>
+            <input type="text" name="username" value="" placeholder="Enter Username">
+            </div>
+            <br>
+            
+            <button type="submit">Submit</button>
+        </div>
+    </form>
     <?php
         $user_id = $_POST['username'];
         $mysqli = new mysqli("mysql.eecs.ku.edu", "m449n496", "Ait3vahs", "m449n496");
         // //if username is empty
-        if($user_id == '') echo "Please submit your username.";
+        if($user_id == '') echo "<div class='noti'>Please submit your username.</div>";
         else{
             
             //check connection
@@ -24,13 +91,13 @@
                 $mysqli->query($query);
                 $result = $mysqli->query($query);              
                 //Username is taken
-                if ($result->num_rows > 0) echo "Username has already been taken.";
+                if ($result->num_rows > 0) echo "<div class='noti'>Username has already been taken.</div>";
                 // Username is not taken
                 else{
                     // Insert username into Users table
                     $sql="INSERT INTO Users (user_id)  VALUES ('$user_id')";  
                     if ($mysqli->query($sql) === TRUE) {
-                        echo "New user created successfully!";
+                        echo "<div class='noti'>New user created successfully!</div>";
                     } else {
                         echo "Error: " . $sql . "<br>" . $mysqli->error;
                     }
@@ -38,6 +105,7 @@
                 $mysqli -> close();
             }
         }
-    ?>    
+    ?>  
+
 </body>
 </html>
